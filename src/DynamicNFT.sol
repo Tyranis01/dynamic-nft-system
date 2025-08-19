48700fe49c017dd980168cf700cb08d65e294ff1
flasharbai-intern <flasharbai@gmail.com>
2025-08-19 16:04:47 -0400

    Edit: Update Imports

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./interfaces/IDataOracle.sol";
+import "./interfaces/IMetadataRenderer.sol";
 
 contract DynamicNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
