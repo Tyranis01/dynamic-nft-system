6c236815b1723aaf51cff422eb75d6b9ac278a14
flasharbai-intern <flasharbai@gmail.com>
2025-08-19 16:09:14 -0400

    Feat: Add userTokens Storage Mapping

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -38,7 +38,8 @@ contract DynamicNFT is ERC721, ERC721URIStorage, Ownable {
 
     // Storage
     mapping(uint256 => NFTState) public nftStates;
-    
+    mapping(address => uint256[]) public userTokens;
+
     // Events
     event TokenMinted(uint256 indexed tokenId, address indexed to);
     event MetadataUpdated(uint256 indexed tokenId, string newMetadata);
