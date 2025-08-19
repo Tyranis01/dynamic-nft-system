6857a376abf7e2c8a10a5cc6076c32d1b477ba95
flasharbai-intern <flasharbai@gmail.com>
2025-08-19 15:21:52 -0400

    Feat: Add Necessary Imports

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 contract DynamicNFT {}
