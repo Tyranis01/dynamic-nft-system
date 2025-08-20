6164d4ef1e50b4fa2ae2bb73a1cc122949f44900
flasharbai-intern <flasharbai@gmail.com>
2025-08-20 11:21:59 -0400

    Feat: Add mint External Admin Func

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -59,4 +59,29 @@ contract DynamicNFT is ERC721, ERC721URIStorage, Ownable {
         timeOracle = IDataOracle(_timeOracle);
         metadataRenderer = IMetadataRenderer(_metadataRenderer);
     }
+
+    function mint(address to) public onlyOwner returns (uint256) {
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "Max supply reached");
+        
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        
+        _safeMint(to, tokenId);
+        
+        // Initialize NFT state
+        nftStates[tokenId] = NFTState({
+            lastWeatherUpdate: block.timestamp,
+            lastTimeUpdate: block.timestamp,
+            userActionCount: 0,
+            currentWeather: "sunny",
+            currentTimeOfDay: _getCurrentTimeOfDay(),
+            owner: to,
+            createdAt: block.timestamp
+        });
+        
+        userTokens[to].push(tokenId);
+        
+        emit NFTMinted(tokenId, to);
+        return tokenId;
+    }
 }
