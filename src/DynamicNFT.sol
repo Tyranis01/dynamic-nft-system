a3cbfc0c5d45712c7d17c19e1beed83c4e7c7f1d
flasharbai-intern <flasharbai@gmail.com>
2025-08-20 11:23:12 -0400

    Feat: Add updateWeather External Func

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -87,4 +87,18 @@ contract DynamicNFT is ERC721, ERC721URIStorage, Ownable {
         emit NFTMinted(tokenId, to);
         return tokenId;
     }
+
+    function updateWeather(uint256 tokenId) external {
+        require(_exists(tokenId), "Token does not exist");
+        require(
+            block.timestamp >= nftStates[tokenId].lastWeatherUpdate + UPDATE_INTERVAL,
+            "Too early to update"
+        );
+        
+        string memory newWeather = weatherOracle.getData();
+        nftStates[tokenId].currentWeather = newWeather;
+        nftStates[tokenId].lastWeatherUpdate = block.timestamp;
+        
+        emit NFTUpdated(tokenId, "weather", newWeather);
+    }
 }
