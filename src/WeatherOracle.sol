1322d3cf81ad5ce24d9bbc4cb98cd1614d9198f5
flasharbai-intern <flasharbai@gmail.com>
2025-08-21 10:41:59 -0400

    Feat: Add WeatherOracle Contract Title Natspec Comments

--- src/WeatherOracle.sol
+++ src/WeatherOracle.sol
@@ -5,4 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IDataOracle.sol";
 
+/**
+ * @title WeatherOracle
+ * @dev Oracle contract that provides weather data for dynamic NFTs
+ * This is a mock implementation - in production you'd integrate with real weather APIs
+ */
 contract WeatherOracle is IDataOracle, Ownable {}
