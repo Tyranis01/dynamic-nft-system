5f4bc0673062e7e13ad3bb7e3c7717a657ab1acb
flasharbai-intern <flasharbai@gmail.com>
2025-08-21 10:41:31 -0400

    Feat: Add Contract Inheritance

--- src/WeatherOracle.sol
+++ src/WeatherOracle.sol
@@ -5,4 +5,4 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IDataOracle.sol";
 
-contract WeatherOracle {}
+contract WeatherOracle is IDataOracle, Ownable {}
