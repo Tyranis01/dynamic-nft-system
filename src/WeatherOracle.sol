ed970b272a1b4a06a17fef1c5b8e83c908eb3c09
flasharbai-intern <flasharbai@gmail.com>
2025-08-21 10:41:14 -0400

    Feat: Add Necessary Imports

--- src/WeatherOracle.sol
+++ src/WeatherOracle.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./interfaces/IDataOracle.sol";
+
 contract WeatherOracle {}
