3348822b4437ce08214d4dc7029aa51a4711ff8a
flasharbai-intern <flasharbai@gmail.com>
2025-08-23 12:04:56 -0400

    Feat: Add Constant Var STALE_DATA_THRESHOLD

--- src/WeatherOracle.sol
+++ src/WeatherOracle.sol
@@ -39,4 +39,6 @@ contract WeatherOracle is IDataOracle, Ownable {
     event WeatherUpdated(string condition, int256 temperature, uint256 timestamp);
     event UpdaterAuthorized(address indexed updater, bool authorized);
 
+    uint256 public constant STALE_DATA_THRESHOLD = 4 hours;
+
 }
