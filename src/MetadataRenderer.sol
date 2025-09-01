b4f5003b1ebef8f9f77c6d9e8c6ebf93aa184bb4
flasharbai-intern <flasharbai@gmail.com>
2025-09-01 00:46:56 -0400

    Feat: Add MetadataRenderer Natspec Title Comments

--- src/MetadataRenderer.sol
+++ src/MetadataRenderer.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title MetadataRenderer
+ * @dev Generates dynamic metadata and SVG images for NFTs based on their state
+ */
 contract MetadataRenderer {}
