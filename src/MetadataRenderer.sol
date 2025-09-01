7356bc86273e97ec584a31a5f567732bc0c4d235
flasharbai-intern <flasharbai@gmail.com>
2025-09-01 00:47:29 -0400

    Feat: Add Contract Inheritance

--- src/MetadataRenderer.sol
+++ src/MetadataRenderer.sol
@@ -10,4 +10,4 @@ import "./interfaces/IMetadataRenderer.sol";
  * @title MetadataRenderer
  * @dev Generates dynamic metadata and SVG images for NFTs based on their state
  */
-contract MetadataRenderer {}
+contract MetadataRenderer is IMetadataRenderer, Ownable {}
