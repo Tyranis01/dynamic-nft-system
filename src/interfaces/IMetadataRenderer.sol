55dcbfe19b539c941bafa65afd1c0dbcf849e465
flasharbai-intern <flasharbai@gmail.com>
2025-09-10 23:05:38 -0700

    feat: Add struct NFTState In Interface IMetadataRenderer

--- src/interfaces/IMetadataRenderer.sol
+++ src/interfaces/IMetadataRenderer.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface IMetadataRenderer {}
+interface IMetadataRenderer {
+    struct NFTState {
+        uint256 lastWeatherUpdate;
+        uint256 lastTimeUpdate;
+        uint256 userActionCount;
+        string currentWeather;
+        string currentTimeOfDay;
+        address owner;
+        uint256 createdAt;
+    }
+}
